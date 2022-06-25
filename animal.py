9c13e30b4d34395ae1bf160467079a83218ca2fb
khansomm <108038321+khansomm@users.noreply.github.com>
2022-06-24 21:56:17 -0500

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,6 @@
+# Author: Sommia Khan
+# GitHub username: khansomm2
+# Date: 6/24/2022
+# Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
+fave_animal = input("Please enter your favorite animal:")
+print("Your favorite animal is the " + fave_animal + ".")
